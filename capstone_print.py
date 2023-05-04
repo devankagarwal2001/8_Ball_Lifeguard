18e9b3bb6ee7e93e19f1c1f4dc95fb138c88e8f4
jrager5871 <72706225+jrager5871@users.noreply.github.com>
2023-05-03 23:09:43 -0400

    Added python script to write to file
    Jimmy is an idiot

---
+++ capstone_print.py
@@ -0,0 +1,31 @@
+import serial
+
+arduino_port = "/dev/cu.usbserial-AQ01LH22" #serial port of Arduino
+baud = 115200 #arduino uno runs at 9600 baud
+fileName="values.txt" #name of the CSV file generated
+
+ser = serial.Serial(arduino_port, baud)
+print("Connected to Arduino port:" + arduino_port)
+file = open(fileName, "a")
+print("Created file")
+
+samples = 10 #how many samples to read 
+samples *= 5 #each sample is 5 lines
+samples += 10 #startup takes 10 lines
+print_labels = False
+line = 0 #start at 0 because our header is 0 (not real data)
+
+
+
+# collect the samples
+
+
+while(True):
+    getData = ser.readline()
+    dataString = getData.decode('utf-8')
+    #data = dataString[0:]
+    data = getData.decode('utf-8')
+    print(data)
+    with open(fileName, 'w', encoding='UTF8', newline='\n') as f:
+        f.write(data)
+    file.close()
